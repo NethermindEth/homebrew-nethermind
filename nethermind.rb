f7d7ecec43ff309ecbd2af49f6d523f36e19e0a0
Swaraj Phadtare <1swarajphadtare@gmail.com>
2021-08-03 03:28:11 +0530

    sha256 fix

--- nethermind.rb
+++ nethermind.rb
@@ -2,7 +2,7 @@ class Nethermind < Formula
   desc "Our flagship .NET Core Ethereum client for Linux, Windows, MacOs - full and actively developed"
   homepage "http://downloads.nethermind.io"
   url "https://github.com/NethermindEth/nethermind/releases/download/1.10.79/nethermind-darwin-amd64-1.10.79-e45db5f-20210722.zip"
-  sha256 "e64132e2ee0148c021c50938011225143e7f3de094fcd160715a363218bb8cea"
+  sha256 "3c68f79533822c93444867df3c4d1cd7b5f98f60359a0c4da542f83d06daed96"
   license "NOASSERTION"
 
   depends_on "cmake" => :build
