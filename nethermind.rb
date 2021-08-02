39a5c6e45f6e0f703781afbdc4dff657becc4c6d
Swaraj Phadtare <1swarajphadtare@gmail.com>
2021-08-03 02:42:43 +0530

    Init Version on Brew

---
+++ nethermind.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Nethermind < Formula
+  desc "Our flagship .NET Core Ethereum client for Linux, Windows, MacOs - full and actively developed"
+  homepage "http://downloads.nethermind.io"
+  url "https://github.com/NethermindEth/nethermind/archive/refs/tags/1.10.79.tar.gz"
+  sha256 "e64132e2ee0148c021c50938011225143e7f3de094fcd160715a363218bb8cea"
+  license "GNU LESSER GENERAL PUBLIC LICENSE"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install Dir["./Nethermind.Runner"]
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test nethermind`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
+
