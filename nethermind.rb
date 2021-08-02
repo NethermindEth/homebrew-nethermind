68746e790ed21828051e45492a3fe862454b65b0
Swaraj Phadtare <1swarajphadtare@gmail.com>
2021-08-03 02:50:34 +0530

    Nethermind directory fix

--- nethermind.rb
+++ nethermind.rb
@@ -16,7 +16,7 @@ class Nethermind < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    bin.install "nethermind"
+    # bin.install "nethermind"
     bin.install Dir["./Nethermind.Runner"]
   end
 
