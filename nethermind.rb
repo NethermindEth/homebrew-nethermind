51ca92d94bc5da209ee4d30594e3f24817e69fc3
Swaraj Phadtare <1swarajphadtare@gmail.com>
2021-08-03 14:17:05 +0530

    Renaming Nethermind.Runner

--- nethermind.rb
+++ nethermind.rb
@@ -3,17 +3,16 @@ class Nethermind < Formula
   homepage "http://downloads.nethermind.io"
   url "https://github.com/NethermindEth/nethermind/releases/download/1.10.79/nethermind-darwin-amd64-1.10.79-e45db5f-20210722.zip"
   sha256 "3c68f79533822c93444867df3c4d1cd7b5f98f60359a0c4da542f83d06daed96"
-  license "NOASSERTION"
-
-  depends_on "cmake" => :build
+  license "GNU LESSER GENERAL PUBLIC LICENSE"
 
   def install
+    File.rename("./Nethermind.Runner","./nethermind")
     bin.install Dir['./chainspec']
     bin.install Dir['./configs']
     bin.install Dir['./Data']
     bin.install Dir['./keystore']
     bin.install Dir['./plugins']
-    bin.install Dir['./Nethermind.Runner']
+    bin.install Dir['./nethermind']
     bin.install Dir['./Nethermind.Cli']
     bin.install Dir['./Nethermind.Launcher']
     bin.install Dir['./NLog.config']
