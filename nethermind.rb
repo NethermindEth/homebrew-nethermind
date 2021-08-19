9005ddbd9899cb2316f60f7bdc16bddb044bc68e
1swaraj <36474630+1swaraj@users.noreply.github.com>
2021-08-19 20:30:44 +0530

    Update nethermind.rb

--- nethermind.rb
+++ nethermind.rb
@@ -2,7 +2,7 @@ class Nethermind < Formula
   desc "Our flagship .NET Core Ethereum client for Linux, Windows, MacOs - full and actively developed"
   homepage "http://downloads.nethermind.io"
   url "https://github.com/NethermindEth/nethermind/releases/download/1.11.0/nethermind-darwin-amd64-1.11.0-eb0ea65-20210819.zip"
-  sha256 "eba9735f22c3ec396b7aaedc65079747535ecf0d721ebe24013f73b418f658da"
+  sha256 "45f38bcce851401734e20fb019c7ae209b210d55e1fc772b374825db0fe4ea13"
   license "GNU LESSER GENERAL PUBLIC LICENSE"
 
   def install
