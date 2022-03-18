50b64da70ae1a0aa35f3bd45ca4f6fd00eab3592
Mateusz Jędrzejewski <33068017+matilote@users.noreply.github.com>
2022-03-18 13:23:11 +0100

    arm64 support

--- nethermind.rb
+++ nethermind.rb
@@ -1,9 +1,24 @@
 class Nethermind < Formula
+  app_version = "1.12.6"
+  commit = "fb2a395"
+  date = "20220317"
   desc "Our flagship .NET Core Ethereum client for Linux, Windows, MacOs - full and actively developed"
   homepage "http://downloads.nethermind.io"
   url "https://github.com/NethermindEth/nethermind/releases/download/1.12.4/nethermind-darwin-amd64-1.12.4-1c8b669-20220113.zip"
   sha256 "8fbf4ccdb6fe97884bfaa0e1df431e4b11808b564e80d0a9945d2d736b1d4752"
   license "GNU LESSER GENERAL PUBLIC LICENSE"
+  version app_version
+  
+  case
+  when OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/nethermind-darwin-amd64-#{app_version}-#{commit}-#{date}.zip"
+    sha256 "df6555a27218344c53e2094884750b47f347392decefd2c58e4757d0b43bdb92"
+  when OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/nethermind-darwin-arm64-#{app_version}-#{commit}-#{date}.zip"
+    sha256 "1b5a968ca6d5bf2b951f63ea333777fda33b346821655db4476e6e55170a8cec"
+  else
+    odie "Unexpected platform!"
+  end
 
   def install
     File.rename("./Nethermind.Runner","./nethermind")
