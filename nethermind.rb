d04f188ef930478dbca8095b65f5333b14afb8c5
kamilchodola <kamilchodola@users.noreply.github.com>
2022-10-29 09:32:50 +0000

    Update Homebrew to latest release

--- nethermind.rb
+++ nethermind.rb
@@ -1,7 +1,7 @@
 class Nethermind < Formula
-  app_version = '1.14.3'
-  commit = '7074612'
-  date = '20221003'
+  app_version = '1.14.5'
+  commit = '380bf9c'
+  date = '20221029'
   desc "Our flagship .NET Core Ethereum client for Linux, Windows, MacOs - full and actively developed"
   homepage "http://downloads.nethermind.io"
   license "GNU LESSER GENERAL PUBLIC LICENSE"
@@ -10,10 +10,10 @@ class Nethermind < Formula
   case
   when OS.mac? && Hardware::CPU.intel?
     url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/nethermind-darwin-amd64-#{app_version}-#{commit}-#{date}.zip"
-    sha256 "6c7a137be7e8ca3bee273a567ddba9155c5a2a933690b9b4ce8dd35da7133fe3"
+    sha256 "6b272bf076007e5ce351329bd052589b05f1cf7a0a5f763f0e9fd8b28b365eb7"
   when OS.mac? && Hardware::CPU.arm?
     url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/nethermind-darwin-arm64-#{app_version}-#{commit}-#{date}.zip"
-    sha256 "bb8030686054d8cd05148a2c7a38e68e0fb573e1a65f27a54955a874511fba64"
+    sha256 "d38a9eb41b7c65dda59a8a64ef418610821e4f137ddfeca2557fefc74440464f"
   else
     odie "Unexpected platform!"
   end
