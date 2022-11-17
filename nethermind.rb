d03450bc74d2e12ed768ef33be93421fa5a3a189
Ruben Buniatyan <rubo@users.noreply.github.com>
2022-11-17 01:18:00 +0100

    Revise and update package file names

--- nethermind.rb
+++ nethermind.rb
@@ -1,18 +1,17 @@
 class Nethermind < Formula
   app_version = '1.14.5'
-  commit = '380bf9c'
-  date = '20221029'
-  desc "Our flagship .NET Core Ethereum client for Linux, Windows, MacOs - full and actively developed"
-  homepage "http://downloads.nethermind.io"
-  license "GNU LESSER GENERAL PUBLIC LICENSE"
+  package_prefix = ''
+  desc "Our flagship Ethereum client for Linux, Windows, and macOS—full and actively developed."
+  homepage "https://downloads.nethermind.io"
+  license "LGPL-3.0-only"
   version app_version
   
   case
   when OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/nethermind-darwin-amd64-#{app_version}-#{commit}-#{date}.zip"
+    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/#{package_prefix}-osx-x64.zip"
     sha256 "6b272bf076007e5ce351329bd052589b05f1cf7a0a5f763f0e9fd8b28b365eb7"
   when OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/nethermind-darwin-arm64-#{app_version}-#{commit}-#{date}.zip"
+    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/#{package_prefix}-osx-arm64.zip"
     sha256 "d38a9eb41b7c65dda59a8a64ef418610821e4f137ddfeca2557fefc74440464f"
   else
     odie "Unexpected platform!"
