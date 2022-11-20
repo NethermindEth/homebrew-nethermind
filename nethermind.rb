3fbd3a8a6d4926c1dc821621698f6a9af2a6e619
Ruben Buniatyan <rubo@users.noreply.github.com>
2022-11-20 23:07:45 +0100

    Update package file names

--- nethermind.rb
+++ nethermind.rb
@@ -8,10 +8,10 @@ class Nethermind < Formula
   
   case
   when OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/#{package_prefix}-osx-x64.zip"
+    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/#{package_prefix}-macos-x64.zip"
     sha256 "6b272bf076007e5ce351329bd052589b05f1cf7a0a5f763f0e9fd8b28b365eb7"
   when OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/#{package_prefix}-osx-arm64.zip"
+    url "https://github.com/NethermindEth/nethermind/releases/download/#{app_version}/#{package_prefix}-macos-arm64.zip"
     sha256 "d38a9eb41b7c65dda59a8a64ef418610821e4f137ddfeca2557fefc74440464f"
   else
     odie "Unexpected platform!"
