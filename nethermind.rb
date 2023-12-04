f49491f79dfca1f7a9b94858c72a99ba92203fd8
nethermind-machine <nethermind-machine@users.noreply.github.com>
2023-12-04 13:11:53 +0000

    Update Homebrew formula

--- nethermind.rb
+++ nethermind.rb
@@ -2,15 +2,15 @@ class Nethermind < Formula
   desc "A robust execution client for Ethereum node operators."
   homepage "https://nethermind.io/nethermind-client"
   license "LGPL-3.0-only"
-  version "1.22.0"
+  version "1.23.0"
   
   case
   when OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/NethermindEth/nethermind/releases/download/1.22.0/nethermind-1.22.0-ae444a4b-macos-x64.zip"
-    sha256 "0e82251daede4af88a7ee964d77f94ccfbe31264149081d186666938cd234208"
+    url "https://github.com/NethermindEth/nethermind/releases/download/1.23.0/nethermind-1.23.0-bcd3b8ae-macos-x64.zip"
+    sha256 "7ab19e917aa99d96c5d76c8c01256f3617f2e43527cd977dccf4db137bdfe8e5"
   when OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/NethermindEth/nethermind/releases/download/1.22.0/nethermind-1.22.0-ae444a4b-macos-arm64.zip"
-    sha256 "0e82251daede4af88a7ee964d77f94ccfbe31264149081d186666938cd234208"
+    url "https://github.com/NethermindEth/nethermind/releases/download/1.23.0/nethermind-1.23.0-bcd3b8ae-macos-arm64.zip"
+    sha256 "65b844b3720bfdb1565e6eac071ee8e0c174ad79aee1d760d68218e6e2689750"
   else
     odie "Platform not supported"
   end
