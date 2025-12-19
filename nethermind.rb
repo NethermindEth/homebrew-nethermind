3940d3b3d296d0409aaffa262c0d0b75163a150b
stdevMac <stdevMac@users.noreply.github.com>
2025-12-19 13:29:31 +0000

    Update for v1.35.8

--- nethermind.rb
+++ nethermind.rb
@@ -2,15 +2,15 @@ class Nethermind < Formula
   desc "A robust execution client for Ethereum node operators."
   homepage "https://nethermind.io/nethermind-client"
   license "LGPL-3.0-only"
-  version "1.35.7"
+  version "1.35.8"
   
   case
   when OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/NethermindEth/nethermind/releases/download/1.35.7/nethermind-1.35.7-92050c1f-macos-x64.zip"
-    sha256 "c14e8b1e4e6a3abfbb58d84aef19d30221f49540242c90083d3b182e4b5aa783"
+    url "https://github.com/NethermindEth/nethermind/releases/download/1.35.8/nethermind-1.35.8-c066aee2-macos-x64.zip"
+    sha256 "590c02f5358667abd0ffbe70a0d883b88adf9a0c71108f5a93e476210853763e"
   when OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/NethermindEth/nethermind/releases/download/1.35.7/nethermind-1.35.7-92050c1f-macos-arm64.zip"
-    sha256 "49af432a44debc365f628d203d27380d56ee6f111e4d7a2fb63a59164250ae9c"
+    url "https://github.com/NethermindEth/nethermind/releases/download/1.35.8/nethermind-1.35.8-c066aee2-macos-arm64.zip"
+    sha256 "75db8b6172b4f1e69095e26013a907f4a67c49f1e4ae6889669de1fb4e89dee8"
   else
     odie "Platform not supported"
   end
